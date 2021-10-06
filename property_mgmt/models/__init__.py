94a39a2edadf8af1a883c3a0986b9bfea5ba3d68
Miracle Ayodele <miracleaayodele@gmail.com>
2021-10-06 09:24:42 +0100

    clearedup main branch

--- property_mgmt/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import property
-from . import res_partner
--- property_mgmt/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    property_stakeholder_ids = fields.One2many('property.stakeholder', 'partner_id', string="Properties")
