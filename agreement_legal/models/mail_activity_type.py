b1d682bbaceed55d8463ab2ce8035faabcd7fa0e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-29 11:40:15 +0100

    Delete agreement_legal directory
    OCA repo

--- agreement_legal/models/mail_activity_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class MailActivityTypeResModel(models.Model):
-    _inherit = "mail.activity.type"
-
-    res_model_id = fields.Many2one('ir.model', 'Document Model', index=True, ondelete='cascade', required=True)
-
