b27c5ab8adbc46656bb85a8587cdc52ef22c0fb3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-30 14:02:28 +0200

    Update __manifest__.py

--- property_mgmt/__manifest__.py
+++ property_mgmt/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Property: Mgmt',
-    'version': '16.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '0.0.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'This module is used to manage properties and link it to a partner.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
