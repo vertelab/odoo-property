1be882b378f046c06ddb75251f36310924fc7518
iammiracle <miracleaayodele@gmail.com>
2025-09-17 08:27:31 +0200

    property maintenance

---
+++ property_maintanance/models/maintenance_equipment.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api, _
+import requests
+import json
+from datetime import datetime
+import ast
+
+
+class Maintenance(models.Model):
+    _inherit = "maintenance.equipment"
+
+    property_id = fields.Many2one("property.property", string="Property")
+
+
+
---
+++ property_maintanance/models/property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api, _
+import math
+
+
+class PropertyProperty(models.Model):
+    _inherit = 'property.property'
+
+    maintenance_equipment_id = fields.Many2one("maintenance.equipment", string="Maintenance Equipment")
+
+    def action_view_maintenance_equipment(self):
+        kanban_view_id = self.env.ref('maintenance.hr_equipment_view_kanban').id
+        list_view_id = self.env.ref('maintenance.hr_equipment_view_tree').id
+        return {
+            'name': _("Property Equipments"),
+            'type': 'ir.actions.act_window',
+            'res_model': 'maintenance.equipment',
+            'view_mode': 'kanban,list,form',
+            'views': [(kanban_view_id, 'kanban'), (list_view_id, 'list'), (False, 'form')],
+            'domain': [('property_id', '=', self.id)],
+            'context': {
+                'default_property_id': self.id
+            }
+        }
