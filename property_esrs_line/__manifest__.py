eb922c052af815e8edcb05456a0b58556f966a51
vertelbot <vertelbot@vertel.se>
2025-10-21 11:17:06 +0000

    odoobranchpfile odoo-property from 18.0. Do not p-file sync

--- property_esrs_line/__manifest__.py
+++ property_esrs_line/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Property: Propety and ESRS Line',
-    'version': '18.0.0.0.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'This module is used to manage properties with ESRS Line.',
     # Categories can be used to filter modules in modules listing
